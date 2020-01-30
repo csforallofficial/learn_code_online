b41e96f9c1700df05ba95175e9786a7b559f54de
Sunyul Hossen <60459287+xenon-suny@users.noreply.github.com>
2020-01-30 16:16:16 +0530

    finish README
    code added

---
+++ learn_code_online1.py
@@ -0,0 +1,11 @@
+#learn_code_online
+n=int(input("enter the number whose muliplication table you want to create:"))
+a=1
+b=1
+mul=0
+while(a==1):
+    mul=n*b
+    b+=1
+    print(n,"X",b,"=",mul)
+    
+    
